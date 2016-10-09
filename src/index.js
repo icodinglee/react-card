471fc15f1c7b4bb65d4c80da8e7ec7218bcfa37a
icodinglee <Lihk11844@126.com>
2016-10-09 16:01:11 +0800

    bundle

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom";
-import React, { PropTypes } from 'react';
-import App from './App';
-import './Main.css';
-
-ReactDOM.render(<App />, document.getElementById('app'));
